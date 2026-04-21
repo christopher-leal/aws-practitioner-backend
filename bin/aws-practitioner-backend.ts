c957911a9aa1e8048138578307c141cdb5529a66
Christopher Leal <christopher.leal@coxautoinc.com>
2026-04-20 22:51:38 -0600

    chore: update dependencies in package.json
    - Added tsconfig-paths for improved TypeScript path resolution.
    - Included AWS SDK for DynamoDB and AWS Lambda types for better integration.

--- bin/aws-practitioner-backend.ts
+++ bin/aws-practitioner-backend.ts
@@ -2,9 +2,12 @@
 import * as cdk from "aws-cdk-lib/core";
 import { HelloLambdaStack } from "../lib/hello-lambda/hello-lambda-stack";
 import { ProductServiceStack } from "../lib/product-service-stack/product-service-stack";
+import { TodoStack } from "../lib/todo/TodoStack";
 
 const app = new cdk.App();
 
 new HelloLambdaStack(app, "HelloLambdaStack", {});
 
 new ProductServiceStack(app, "ProductServiceStack", {});
+
+new TodoStack(app, "TodoStack");
