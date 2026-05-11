04a97cd26c1f041ad9ca33dbd1fcf88eb50566ae
Christopher Leal <christopher.leal@coxautoinc.com>
2026-05-11 16:04:23 -0600

    feat: add AuthorizerStack with Cognito user pool and API Gateway integration

--- bin/aws-practitioner-backend.ts
+++ bin/aws-practitioner-backend.ts
@@ -4,6 +4,7 @@ import { ProductServiceStack } from "../lib/product-service-stack/product-servic
 import { ImportServiceStack } from "../lib/product-service-stack/import-service-stack";
 import { ProductSqsStack } from "../lib/product-sqs/product-sqs-stack";
 import { ProductSnsStack } from "../lib/product-sns/product-sns-stack";
+import { AuthorizerStack } from "../lib/authorizer-stack/authorizer-stack";
 
 const app = new cdk.App();
 
@@ -17,3 +18,4 @@ new ProductSqsStack(app, "ProductSqsStack");
 
 new ProductSnsStack(app, "ProductSnsStack");
 
+new AuthorizerStack(app, "AuthorizerStack");
